259403c9cbdc55bc788bd5861d99460a3e12828d
Mahesh Mane <maheshmane9075@gmail.com>
2025-05-22 19:48:01 +0530

    Third Commit

--- FrontEnd/src/Pages/Shop.jsx
+++ FrontEnd/src/Pages/Shop.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
-import Popular from "../Components/Popular/popular";
+import Popular from "../Components/Popular/Popular";
 import Offers from "../Components/Offers/Offers";
 import NewCollection from "../Components/NewCollection/NewCollection";
 import NewsLetter from "../Components/NewsLetter/NewsLetter";
